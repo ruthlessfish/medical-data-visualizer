91fe69b17bc9b47f0aec060ca14699e7666021e6
Shane Pearson <bubbafoley@gmail.com>
2024-04-09 16:13:20 -0500

    adding fcc inspired code and started refactoring

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2) > 25
+df['overweight'] = df['overweight'].astype(int)
 
 # 3
 
+def normalize(value):
+    if value == 1:
+        return 0
+    elif value > 1:
+        return 1
+    return value
+
+df[['cholesterol', 'gluc']] = df[['cholesterol', 'gluc']].applymap(normalize)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=['cardio'],
+                         value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'],
+                         var_name='Feature',
+                         value_name='Value')
 
 
     # 6
-    df_cat = None
-    
+    df_cat.groupby(['cardio', 'variable', 'Value'])['Value'].size().reset_index(name='Count')
+
 
     # 7
-
-
+    g = sns.catplot(x='Feature', y='Count', hue='Value', col='cardio', data=df_cat, kind='bar', height=4, aspect=1.5)
 
     # 8
-    fig = None
+    fig = g.fig
 
 
     # 9
@@ -37,6 +48,7 @@ def draw_cat_plot():
 
 # 10
 def draw_heat_map():
+<<<<<<< HEAD
     # 11
     df_heat = None
 
@@ -57,4 +69,4 @@ def draw_heat_map():
 
     # 16
     fig.savefig('heatmap.png')
-    return fig
+    return fix
